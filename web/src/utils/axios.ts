bedb0ef07c5da615ec3a7a2a4812e85a149c486e
Anwesh <anweshknayak@gmail.com>
2022-10-12 22:39:25 +0530

    feat(axios): change to localhost

--- web/src/utils/axios.ts
+++ web/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://api_explorer:8085/api/v1'
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/api/v1'
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
