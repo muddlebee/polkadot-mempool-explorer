5d8d0c05cc2357aad6c9e70b082ca3ff563bc043
Anwesh <anweshknayak@gmail.com>
2023-02-22 07:44:24 +0530

    fix(axios/dotenv): todo fix env management

--- web/src/utils/axios.ts
+++ web/src/utils/axios.ts
@@ -1,11 +1,8 @@
 import axios from 'axios'
 
-let API_BASE_URL =
+const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/api/v1/mempool-explorer'
-console.log('API_BASE_URL', API_BASE_URL)
-if (process.env.NODE_ENV === 'development') {
-  API_BASE_URL = 'http://localhost:8081/api/v1/mempool-explorer'
-}
+
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   responseType: 'json',
